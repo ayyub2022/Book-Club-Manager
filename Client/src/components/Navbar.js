e259fa31b4a4a0ac135deeb3b3390bb9a56f059c
Kamau <kamauwamuigaijr@gmail.com>
2024-07-24 21:04:14 +0300

    “UPDATEDNavbar”

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import CSS file for styling
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li> {/* Link to Home */}
-        <li><Link to="/register">Register</Link></li> {/* Link to Register */}
-        <li><Link to="/login">Login</Link></li> {/* Link to Login */}
-        <li><Link to="/dashboard">Dashboard</Link></li> {/* Link to Dashboard */}
-        <li><Link to="/books/1">Book Details</Link></li> {/* Link to Book Details - example with bookId 1 */}
-        <li><Link to="/profile">User Profile</Link></li> {/* Link to User Profile */}
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={`${process.env.PUBLIC_URL}/images/book_club.jpg`} alt="Book Club Icon" className="navbar-icon" />
+        <span className="navbar-title">Book Club Manager</span>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/books/1">Book Details</Link></li>
+        <li><Link to="/profile">User Profile</Link></li>
       </ul>
     </nav>
   );
