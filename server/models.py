990126be13e1a8257fd797d9a61733f2acca4100
14justin <justinmwangi40@gmail.com>
2024-07-24 12:36:00 +0300

    book

--- server/models.py
+++ server/models.py
@@ -1,38 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy import func
+import re
+from flask import url_for
 
-db = SQLAlchemy()
+from extensions import db, bcrypt
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules = ('-reviews.user', '-favorite_books.favorited_by')
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    reviews = db.relationship('Review', backref='user', lazy=True)
-    favorite_books = db.relationship('Book', secondary='user_book', lazy='subquery',
-        backref=db.backref('favorited_by', lazy=True))
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password = db.Column(db.String, nullable=False)
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    author = db.Column(db.String(255), nullable=False)
-
-    def __repr__(self):
-        return f'<Book {self.title}>'
-
-class Review(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<Review {self.id}>'
-
-class UserBook(db.Model):
-    __tablename__ = 'user_book'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), primary_key=True)
-
-    def __repr__(self):
-        return f'<UserBook user_id={self.user_id} book_id={self.book_id}>'
+    reviews = db.relationship('Review', back_populates='user')
+    favorite_books = association_proxy('user_books', 'book')
