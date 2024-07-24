2db12d4fc29665f014b97f61f8f2c6c582be1908
Kamau <kamauwamuigaijr@gmail.com>
2024-07-24 13:57:03 +0300

    “homepage”

--- Client/src/components/BookCard.js
+++ Client/src/components/BookCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import './BookCard.css'; // Ensure you have this CSS file
+import './BookCard.css'; // Import the CSS file for styling
 
 function BookCard({ book }) {
   return (
     <div className="book-card">
+      <img src={book.image || 'https://via.placeholder.com/150'} alt={book.title} /> {/* Add an image */}
       <h3>{book.title}</h3>
       <p><strong>Author:</strong> {book.author}</p>
       <p><strong>Genre:</strong> {book.genre}</p>
-      <p>{book.description}</p>
+      <p><strong>Description:</strong> {book.description}</p>
     </div>
   );
 }
