e971bcfa88a0067b482bc209f646be4763be2c23
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-07-24 23:14:32 +0300

    CRUD on books

--- server/app.py
+++ server/app.py
@@ -1,71 +1,67 @@
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
+from flask_restful import Resource, Api 
+from flask_cors import CORS
 from models import db, User, Book, Review, UserBook
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 # Initialize the database and migration tool
 db.init_app(app)
 migrate = Migrate(app, db)
 
-@app.route('/books', methods=['GET', 'POST'])
-def manage_books():
-    if request.method == 'GET':
-        books = Book.query.all()
-        return jsonify([book.to_dict() for book in books])
-    elif request.method == 'POST':
-        data = request.json
-        new_book = Book(title=data['title'], author=data['author'])
+# Instantiate REST API
+api = Api(app)
+# Instantiate CORS
+CORS(app)
+
+
+class Book(Resource):
+    def get(self, id=None):
+        if id is None:
+            books = [book.to_dict() for book in Book.query.all()]
+            return jsonify(books), 200
+        else:
+            book = Book.query.get_or_404(id)
+            return jsonify(book.to_dict()), 200
+
+    def post(self):
+        data = request.get_json()
+        if not data:
+            return {"message": "No input data provided"}, 400
+        
+        new_book = Book(
+            title=data['title'],
+            author=data['author'],
+            genre=data['genre']
+        )
         db.session.add(new_book)
         db.session.commit()
         return jsonify(new_book.to_dict()), 201
 
-@app.route('/books/<int:book_id>', methods=['GET', 'PUT', 'DELETE'])
-def book_detail(book_id):
-    book = Book.query.get_or_404(book_id)
-    if request.method == 'GET':
-        return jsonify(book.to_dict())
-    elif request.method == 'PUT':
-        data = request.json
-        book.title = data['title']
-        book.author = data['author']
+    def put(self, id=None):
+        book = Book.query.get_or_404(id)
+        data = request.get_json()
+        if not data:
+            return {"message": "No input data provided"}, 400
+
+        book.title = data.get('title', book.title)
+        book.author = data.get('author', book.author)
+        book.genre = data.get('genre', book.genre)
+    
         db.session.commit()
-        return jsonify(book.to_dict())
-    elif request.method == 'DELETE':
+        return jsonify(book.to_dict()), 200
+
+    def delete(self, id=None):
+        book = Book.query.get_or_404(id)
         db.session.delete(book)
         db.session.commit()
-        return '', 204
-
-@app.route('/reviews', methods=['POST'])
-def add_review():
-    data = request.json
-    new_review = Review(content=data['content'], rating=data['rating'], book_id=data['book_id'], user_id=data['user_id'])
-    db.session.add(new_review)
-    db.session.commit()
-    return jsonify(new_review.to_dict()), 201
-
-@app.route('/reviews/<int:review_id>', methods=['DELETE'])
-def delete_review(review_id):
-    review = Review.query.get_or_404(review_id)
-    db.session.delete(review)
-    db.session.commit()
-    return '', 204
-
-@app.route('/users/<int:user_id>/favorites', methods=['POST', 'DELETE'])
-def manage_favorites(user_id):
-    data = request.json
-    user = User.query.get_or_404(user_id)
-    if request.method == 'POST':
-        book = Book.query.get_or_404(data['book_id'])
-        user.favorites.append(book)
-        db.session.commit()
-        return jsonify(user.to_dict())
-    elif request.method == 'DELETE':
-        book = Book.query.get_or_404(data['book_id'])
-        user.favorites.remove(book)
-        db.session.commit()
-        return '', 204
+        return {"message": "book deleted"}, 204
+    
+api.add_resource(Book, '/book', '/book/<int:id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
