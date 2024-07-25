afdd677b507e55620f18c322d3314871e444bb86
Edmound Njoori <eddnjoori@gmail.com>
2024-07-25 17:25:48 +0300

    Frontend : Add login and register functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
     // Handle user login logic here
-    console.log('Login', { email, password });
+    try {
+      const res = await fetch("http://localhost:5555/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data);
+      } else if (data.access_token) {
+        console.log(data.access_token)
+        localStorage.setItem("access_token", data.access_token);
+        localStorage.setItem("refresh_token", data.refresh_token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -25,7 +44,9 @@ function Login() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="button" onClick={handleLogin}>Login</button>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
       </form>
     </div>
   );
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,18 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Register() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUserame] = useState(null);
+  const [tocken,setTocken]=useState(null)
 
-  const handleRegister = () => {
+  const handleRegister = async (e) => {
     // Handle user registration logic here
-    console.log('Register', { email, password });
+    e.preventDefault();
+    // Handle user login logic here
+    try {
+      const res = await fetch("http://localhost:5555/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username,email, password }),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="container">
       <form>
         <h2>Register</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUserame(e.target.value)}
+        />
         <input
           type="email"
           placeholder="Email"
@@ -25,7 +49,9 @@ function Register() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="button" onClick={handleRegister}>Register</button>
+        <button type="button" onClick={handleRegister}>
+          Register
+        </button>
       </form>
     </div>
   );
